ef63e7089e8e484b26ed1649008514906b8ec365
Aneta Prokopiuk <anetaprokopiuk@gmail.com>
2018-10-14 12:49:26 +0200

    Zadanie2-2

---
+++ src/main/java/Zadanie1_2.java
@@ -0,0 +1,10 @@
+public class Zadanie1_2 {
+    public static void main(String[] args) {
+        /*for (int i = 0; i < args.length; i++) {
+        /*    System.out.println(args[i]);
+        }*/
+int a = Integer.parseInt(args[0]);
+int b = Integer.parseInt(args[1]);
+        System.out.println(a+ " + " +b+ " = " +(a+b));
+    }
+}
--- src/main/java/Zadanie2.java
+++ src/main/java/Zadanie2.java
@@ -9,13 +9,33 @@ public class Zadanie2 {
         int N;
         System.out.println("Please type in a number: ");
         Scanner scanner = new Scanner(System.in);
-
         N = scanner.nextInt();
-        int a = N/3;
-        int b = N/5;
+        int a = 3;
+        int b = 5;
+        int x = N/3;
+        int y = N/5;
+        int sumthree = 0;
+        int sumfive = 0;
+        int i;
+        /*for (int i=1; a<=N;i++){
+            sum=sum+(3*i);
+            a=a*i;
+            System.out.println(a);
+        }
+        System.out.println("a = "+sum);*/
+        //System.out.println("Wielokrotność 3 w "+N+ " = "+a);
+        //System.out.println("Wielokrotność 5 w "+N+ " = "+b);
+        for (i=0; i <= N;i++){
+            if((i%3)==0){
+                sumthree=sumthree+i;
+            }
+            if((i%5)==0) {
+                sumfive = sumfive + i;
+            }
+        }
+        System.out.println(sumthree+sumfive);
 
-        System.out.println("Wielokrotność 3 w "+N+ " = "+a);
-        System.out.println("Wielokrotność 5 w "+N+ " = "+b);
+
 
 
     }
